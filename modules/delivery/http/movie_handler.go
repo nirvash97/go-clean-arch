e289e75decd90d1c79cb055cdf3cad353ae7fd06
Tossaporn Meesiri <tossaporn.meesiri@gmail.com>
2024-10-22 14:35:26 +0700

    🐳 refactor : update MovieHandler

--- modules/delivery/http/movie_handler.go
+++ modules/delivery/http/movie_handler.go
@@ -3,8 +3,8 @@ package http
 import (
 	"encoding/json"
 	"go-clean-arch/modules/usecase"
-	"log"
 	"net/http"
+	"regexp"
 
 	"github.com/gorilla/mux"
 )
@@ -20,12 +20,19 @@ func NewMovieHandler(uc *usecase.MovieUsecase) *MovieHandler {
 func (h *MovieHandler) GetMovieBylanguage(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	lang := params["language"]
+	// Check param should have only letter [return 400]
+	var regex = regexp.MustCompile(`^[a-zA-Z]+$`)
+	if !regex.MatchString(lang) {
+		http.Error(w, "Parameter must contain only word", http.StatusBadRequest)
+		return
+	}
 
 	movies, err := h.uc.GetMovieByLanguage(lang)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	// Check is empty [return 204]
 	if len(movies) <= 0 {
 		http.Error(w, "no item found", http.StatusNoContent)
 		return
@@ -37,7 +44,6 @@ func (h *MovieHandler) GetMovieBylanguage(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	log.Println(movies)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(decodeData)
 }
