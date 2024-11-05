79bdf1f952f049e67f414af00b2c8938f5a41137
Tossaporn Meesiri <tossaporn.meesiri@gmail.com>
2024-11-05 14:10:26 +0700

    🐳 refactor : update MovieStruct

--- modules/entities/movies/movies.go
+++ modules/entities/movies/movies.go
@@ -4,25 +4,29 @@ import (
 	"time"
 )
 
+// Tips
+// 1.) omitempty will hide struct field when value was nil
+// 2.) prefix struct variable with * mean it be nullable ( back to read first tips )
 type Movie struct {
-	Title           string    `json:"title"`
-	Genres          []string  `json:"genres"`
-	Runtime         int       `json:"runtime"`
-	Imdb            MovieImdb `json:"imdb"`
-	Plot            string    `json:"plot"`
-	Rated           string    `json:"rated"`
-	Cast            []string  `json:"cast"`
-	NumMflixComment int       `json:"num_mflix_comments"`
-	Poster          string    `json:"poster"`
-	Fullplot        string    `json:"fullplot"`
-	Languages       []string  `json:"languages"`
-	Released        time.Time `json:"released"`
-	Directors       []string  `json:"directors"`
-	Writers         []string  `json:"writers"`
+	Title           string     `json:"title"`
+	Genres          []string   `json:"genres,omitempty"`
+	Year            int        `json:"year"`
+	Runtime         int        `json:"runtime,omitempty"`
+	Imdb            *MovieImdb `json:"imdb,omitempty"`
+	Plot            string     `json:"plot,omitempty"`
+	Rated           string     `json:"rated,omitempty"`
+	Cast            []string   `json:"cast,omitempty"`
+	NumMflixComment int        `json:"num_mflix_comments,omitempty"`
+	Poster          string     `json:"poster,omitempty"`
+	Fullplot        string     `json:"fullplot,omitempty"`
+	Languages       []string   `json:"languages,omitempty"`
+	Released        *time.Time `json:"released,omitempty"`
+	Directors       []string   `json:"directors,omitempty"`
+	Writers         []string   `json:"writers,omitempty"`
 }
 
 type MovieImdb struct {
-	Rating float64 `bson:"rating"`
-	Voting int64   `bson:"votes"`
-	Dd     int64   `bson:"id"`
+	Rating float64 `json:"rating,omitempty"`
+	Voting int64   `json:"votes,omitempty"`
+	Dd     int64   `json:"id,omitempty"`
 }
