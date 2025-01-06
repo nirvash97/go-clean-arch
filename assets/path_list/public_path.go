1a9b595096489c2dbb2cacd4aea514f60500c3c5
Tossaporn Meesiri <tossaporn.meesiri@gmail.com>
2025-01-06 11:19:54 +0700

    🐳refactor : update public path

--- assets/path_list/public_path.go
+++ assets/path_list/public_path.go
@@ -4,6 +4,9 @@ func PublicPathConst() []string {
 	publicPath := []string{
 		"/auth/signUp",
 		"/auth/signIn",
+		"/exam/hello",
+		"/exam/user-list",
+		"/exam/user-list/:id",
 	}
 	return publicPath
 }
